c87d12628e66e6a9bac11c21aa89df689ac95acc
Leigh Smith <leigh@leighsmith.com>
2021-01-18 10:58:24 -0500

    Changed to check for .dmg file having been downloaded manually

--- Installer/scripts/extractMAudioFirmware.sh
+++ Installer/scripts/extractMAudioFirmware.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+DMGFILE=~/Downloads/MIDI_USB_OSX_3.5.3.dmg
 # Download MIDI_USB_OSX_3.5.3.dmg
 # https://mac.softpedia.com/get/Drivers/M-Audio-MIDISport-Series.shtml#download
-curl --output /tmp/MIDI_USB_OSX_3.5.3.dmg https://us.softpedia-secure-download.com/dl/c05c654f4d8f1c1d214be8f5e881923e/6004d9a1/400050371/mac/Drivers/MIDI_USB_OSX_3.5.3.dmg
+# curl --output $DMGFILE https://us.softpedia-secure-download.com/dl/c05c654f4d8f1c1d214be8f5e881923e/6004d9a1/400050371/mac/Drivers/MIDI_USB_OSX_3.5.3.dmg
 # TODO check the file downloaded ok.
-if [ -f /tmp/MIDI_USB_OSX_3.5.3.dmg ]; then
-    echo "Downloaded the M-Audio driver, now extracting the firmware files."
-    # Mount MIDI_USB_OSX_3.5.3.dmg
-    hdiutil attach /tmp/MIDI_USB_OSX_3.5.3.dmg
+if [ -f $DMGFILE ]; then
+    echo "Found the M-Audio driver, now extracting the firmware files."
+    # Mount the DMG file
+    hdiutil attach $DMGFILE
     # Extract the .rsrc file
     /usr/bin/xar -x --exclude Distribution --exclude Resources -f "/Volumes/M-Audio USB MIDI Support 3.5.3/M-Audio USB MIDI Support Installer.pkg"
     gunzip -c - < com.m-audio.usbmidisupport.macos.installer_0.pkg/Payload | cpio -i -d "./Library/StartupItems/M-Audio Firmware Loader/MA Firmware Loader.rsrc"
@@ -15,11 +16,11 @@ if [ -f /tmp/MIDI_USB_OSX_3.5.3.dmg ]; then
     /usr/bin/DeRez "./Library/StartupItems/M-Audio Firmware Loader/MA Firmware Loader.rsrc" -useDF > MA_Firmware_Loader.rsrc.decoded
     python rsrc2ihex.py < MA_Firmware_Loader.rsrc.decoded
     # TODO copy a subset of the generated .ihx files
-    sudo cp *.ihx /usr/local/etc/midisport_firmware/
+    sudo mv *.ihx /usr/local/etc/midisport_firmware/
     # Now unmount the disk image.
     rm -r ./Library
     rm MA_Firmware_Loader.rsrc.decoded
     umount "/Volumes/M-Audio USB MIDI Support 3.5.3/"
 else
-    echo "Unable to download the M-Audio driver"
+    echo "Unable to find and extract the M-Audio driver firmware."
 fi
