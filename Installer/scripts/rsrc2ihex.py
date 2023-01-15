c080bca9995853595f774540cf37243c1d607b6a
Leigh Smith <leigh@leighsmith.com>
2023-01-15 10:45:39 -0500

    Upgraded rsrc2ihex.py to run on Python v3, now the default for MacOS 12 etc distributions

--- Installer/scripts/rsrc2ihex.py
+++ Installer/scripts/rsrc2ihex.py
@@ -1,11 +1,24 @@
-#!/bin/env python
+#!/bin/env python3
+#
+# Converts the output of MacOS DeRez utility into Intel Hex files.
+#
+# Leigh Smith <leigh@leighsmith.com>
+#
+# Copyright (c) 2022 LeighSmith.com Inc. All Rights Reserved.
+# Permission is granted to use and modify this code for commercial and
+# non-commercial purposes so long as the author attribution and this
+# copyright message remains intact and accompanies all derived code.
+#
 
-# Converts the output of DeRez into Intel Hex files.
 import fileinput
 import re
 
-firmwareStartRE = re.compile("""data 'FIRM' \([0-9]+, \"(.*)\",.*\)""")
-hexStringRE = re.compile('\s*\$\"([0-9A-F\s]+)\"')
+# Because the decoded DeRez files produce non-ASCII strings in the text interpretation
+# field, we need to read the file as a binary file, which results in the strings being
+# bytes, rather than encoded (i.e. UTF-8) text. So the regular expressions also need to be
+# of the bytes type.
+firmwareStartRE = re.compile(b"""data 'FIRM' \([0-9]+, \"(.*)\",.*\)""")
+hexStringRE = re.compile(b'\s*\$\"([0-9A-F\s]+)\"')
 
 def interpretFirmwareFile(inputLineGenerator):
     """
@@ -19,15 +32,16 @@ def interpretFirmwareFile(inputLineGenerator):
         firmRecordMatch = firmwareStartRE.match(inputLine)
         bytesLineMatch = hexStringRE.match(inputLine)
         if firmRecordMatch:
-            # Catch the firmware starting record
-            firmwareName = firmRecordMatch.group(1)
+            # Catch the firmware starting record.
+            # Encode the filename as ASCII to avoid "b" being prepended.
+            firmwareName = str(firmRecordMatch.group(1), encoding ='ascii')
             firmwareBytes = ''
             inFirmware = True
         elif bytesLineMatch and inFirmware:
-            # Strip out hex text between $"", removing spaces.
-            bytesInHex = bytesLineMatch.group(1).replace(" ", "")
+            # Strip out hex text between $"", removing spaces. Convert to a string.
+            bytesInHex = str(bytesLineMatch.group(1), encoding ='ascii').replace(" ", "")
             firmwareBytes += bytesInHex
-        elif inputLine == '};' and inFirmware:
+        elif inputLine == b'};' and inFirmware:
             # Neither the starting firmware record, nor the bytes lines, so it's the end, so yield the combination.
             inFirmware = False
             yield firmwareName, firmwareBytes
@@ -45,8 +59,7 @@ def calculateChecksum(hexLine):
 
 def writeIntelHexFile(fileName, hexString):
     """
-    Write out the the hex string as an Intel Hex format file, including calculating
-    checksums etc.
+    Write out the the hex string as an Intel Hex format file, including calculating checksums etc.
     """
     with open(fileName, 'w') as fileHandle:
         # partition the hex string
@@ -62,9 +75,11 @@ def writeIntelHexFile(fileName, hexString):
             hexString = hexString[charLen:]
 
 if __name__ == '__main__':
-    for firmwareName, byteString in interpretFirmwareFile(fileinput.input()):
-        print(firmwareName)
+    for firmwareName, byteString in interpretFirmwareFile(fileinput.input(mode='rb')):
         # Strip off the first "FFFF"
         strippedByteString = byteString[4:]
         firmwareFilename = firmwareName + '.ihx'
-        writeIntelHexFile(firmwareFilename, strippedByteString)
+        # Only write out non-empty byte strings to Intel Hex files.
+        if len(strippedByteString) > 0:
+            print(f"Writing {firmwareFilename}")
+            writeIntelHexFile(firmwareFilename, strippedByteString)
